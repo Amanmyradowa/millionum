d339e95982de784a92b8121bae9f5f3672aba2a1
Imdat Developers <imdatdevelopers@Imdats-iMac.local>
2023-10-18 10:48:13 +0500

    main page finally

---
+++ assets/js/swiper/swiper.factory.js
@@ -0,0 +1,13 @@
+new Swiper('.factory__swiper', {
+  slidesPerView: 4,
+  slidesPerGroup: 1,
+  loopedSlides: 1,
+  spaceBetween: 22,
+  grabCursor: true,
+  speed: 1000,
+
+  navigation: {
+    nextEl: '.base-slider-arrows__arrow--type-next-factory',
+    prevEl: '.base-slider-arrows__arrow--type-previous-factory',
+  },
+});
